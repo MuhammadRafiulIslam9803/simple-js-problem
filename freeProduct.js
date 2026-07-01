fe7a93fd6bfc1d2f1a4dd053158736989a318e5d
Rafiul Islam <www.rafiulislam1998@gmail.com>
2026-07-02 00:10:30 +0600

    added simple if else condtion

---
+++ freeProduct.js
@@ -0,0 +1,8 @@
+let bargarPrice = 4500 ;
+
+if (bargarPrice > 500) {
+    console.log("we have a offer get a free drink") ;
+}
+else {
+    console.log("if you want to get a free drink then you have to pay 50 more") ;
+}
