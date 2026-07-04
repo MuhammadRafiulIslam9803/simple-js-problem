a8a709626c7f3e449c052aa71b149550156c96cd
Rafiul Islam <www.rafiulislam1998@gmail.com>
2026-07-04 14:47:56 +0600

    genarate gmail cration from the object element

---
+++ javascriptL3/emailCreate.js
@@ -0,0 +1,7 @@
+let student = { name: "mewo" , roll: 96 ,department: "cse"  }
+
+// jhankar1014.cse@ph.ac.bd
+
+let email = `${student.name}${student.roll}.${student.department}@ph.ac.bd`
+
+console.log(`The email of the student is: ${email}`)
