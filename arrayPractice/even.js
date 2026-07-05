65ac848d9ba1de6fffc3b78c2a6cb99c1579f8e8
Rafiul Islam <www.rafiulislam1998@gmail.com>
2026-07-05 18:21:38 +0600

    find even number from an array

---
+++ arrayPractice/even.js
@@ -0,0 +1,9 @@
+const numbers = [12, 98, 5, 41, 23, 78, 46];
+
+let evenNumbers = [];
+for (const number of numbers) {
+    if (number % 2 === 0) {
+        evenNumbers.push(number);
+    }
+}
+console.log(evenNumbers); // [12, 98, 78, 46]
