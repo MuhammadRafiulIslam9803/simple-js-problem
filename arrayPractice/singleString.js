3a259746c8c7184ed99c7eb8a17f16e788eee78f
Rafiul Islam <www.rafiulislam1998@gmail.com>
2026-07-05 18:24:31 +0600

    added single string from an Array

---
+++ arrayPractice/singleString.js
@@ -0,0 +1,7 @@
+var numbers = ['Tom', 'Tim', 'Tin', 'Tik']
+
+var singleString = numbers.join('');
+console.log(singleString); // "TomTimTinTik"
+
+var singleString = numbers.join(' ');
+console.log(singleString); // "Tom Tim Tin Tik"
